e788c86ed9359654dcc3ac328bbf6b6633bc6f4e
Stéphane de Wit <contact@stephanedewit.be>
2021-09-19 15:33:35 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const http = require('http');
-const crypto = require('crypto')
+const http = require("http");
+const crypto = require("crypto");
 
 const port = process.env.PORT;
-if (!port)
-	throw new Error("Can't read port configuration");
+if (!port) {
+    throw new Error("Can't read port configuration");
+}
 
-http.createServer(function (req, res) {
-  const hash = crypto.createHash('md5');
+http.createServer((req, res) => {
+    const hash = crypto.createHash("md5");
 
-  req.on('data', function(chunk) {
-    hash.update(chunk);
-  });
+    req.on("data", chunk => {
+        hash.update(chunk);
+    });
 
-  req.on('end', function() {
-    const digest = hash.digest("hex");
+    req.on("end", () => {
+        const digest = hash.digest("hex");
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(digest + '\n');
-  });
-}).listen(port, '0.0.0.0');
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end(`${digest}\n`);
+    });
+}).listen(port, "0.0.0.0");
 
-console.log('Server running at http://0.0.0.0:' + port);
+console.log(`Server running at http://0.0.0.0:${port}`);
